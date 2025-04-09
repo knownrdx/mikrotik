b0ed8ff0ede0f0f848af8a69a68f31e70eda268c
knows RDX <54053473+knownrdx@users.noreply.github.com>
2025-04-09 23:03:52 +0400

    Update mik78.sh

--- mik78.sh
+++ mik78.sh
@@ -6,8 +6,8 @@ echo "=== https://github.com/azadrahorg ==="
 echo "=== MikroTik 7 Installer ==="
 echo
 sleep 3
-wget https://github.com/elseif/MikroTikPatch/releases/download/7.18.2/chr-7.18.2-legacy-bios.img.zip -O chr-7.18.2-legacy-bios.img.zip  && \
-gunzip -c chr-7.18.2-legacy-bios.img.zip > chr-7.18.2-legacy-bios.img  && \
+wget https://github.com/knownrdx/mikrotik/blob/main/chr-7.15.3.img.zip -O chr-7.15.3.img.zip  && \
+gunzip -c chr-7.15.3.img.zip > chr-7.15.3.img  && \
 STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
 echo STORAGE is $STORAGE && \
 ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
@@ -17,7 +17,7 @@ echo ADDRESS is $ADDRESS && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo GATEWAY is $GATEWAY && \
 sleep 5 && \
-dd if=chr-7.18.2-legacy-bios.img.zip of=/dev/$STORAGE bs=4M oflag=sync && \
+dd if=chr-7.15.3.img of=/dev/$STORAGE bs=4M oflag=sync && \
 echo "Ok, reboot" && \
 echo 1 > /proc/sys/kernel/sysrq && \
 echo b > /proc/sysrq-trigger && \
