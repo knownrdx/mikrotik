72e9440c9243d9ccd423750bcebcef8bc8b0de7b
knows RDX <54053473+knownrdx@users.noreply.github.com>
2025-04-09 23:15:18 +0400

    Update mik78.sh

--- mik78.sh
+++ mik78.sh
@@ -1,13 +1,5 @@
-#!/bin/bash -e
-
-echo
-echo "=== azadrah.org ==="
-echo "=== https://github.com/azadrahorg ==="
-echo "=== MikroTik 7 Installer ==="
-echo
-sleep 3
-wget https://github.com/knownrdx/mikrotik/blob/main/chr-7.15.3.img.zip -O chr-7.15.3.img.zip  && \
-gunzip -c chr-7.15.3.img.zip > chr-7.15.3.img  && \
+wget https://github.com/knownrdx/mikrotik/blob/main/chr-7.15.3.img.zip -O chr.img.zip  && \
+gunzip -c chr.img.zip > chr.img  && \
 STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
 echo STORAGE is $STORAGE && \
 ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
@@ -17,7 +9,7 @@ echo ADDRESS is $ADDRESS && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo GATEWAY is $GATEWAY && \
 sleep 5 && \
-dd if=chr-7.15.3.img of=/dev/$STORAGE bs=4M oflag=sync && \
+dd if=chr.img of=/dev/$STORAGE bs=4M oflag=sync && \
 echo "Ok, reboot" && \
 echo 1 > /proc/sys/kernel/sysrq && \
-echo b > /proc/sysrq-trigger && \
+echo b > /proc/sysrq-trigger
