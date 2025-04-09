33a6cf683d59ee03c9f6d03193037e9480586e38
knows RDX <54053473+knownrdx@users.noreply.github.com>
2025-04-09 23:34:29 +0400

    Update mik78.sh

--- mik78.sh
+++ mik78.sh
@@ -1,23 +1,54 @@
-#!/bin/bash -e
+#!/bin/bash
 
-echo
-echo "=== azadrah.org ==="
-echo "=== https://github.com/azadrahorg ==="
-echo "=== MikroTik 7 Installer ==="
-echo
+echo "Starting MikroTik CHR 7.15.3 installation..."
 sleep 3
-wget https://github.com/knownrdx/mikrotik/blob/main/chr-7.15.3.img.zip -O chr-7.15.3.img.zip  && \
-unzip -c chr-7.15.3.img.zip > chr-7.15.3.img  && \
-STORAGE=`lsblk | grep disk | cut -d ' ' -f 1 | head -n 1` && \
-echo STORAGE is $STORAGE && \
-ETH=`ip route show default | sed -n 's/.* dev \([^\ ]*\) .*/\1/p'` && \
-echo ETH is $ETH && \
-ADDRESS=`ip addr show $ETH | grep global | cut -d' ' -f 6 | head -n 1` && \
-echo ADDRESS is $ADDRESS && \
-GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
-echo GATEWAY is $GATEWAY && \
-sleep 5 && \
-dd if=chr-7.15.3.img of=/dev/$STORAGE bs=4M oflag=sync && \
-echo "Ok, reboot" && \
-echo 1 > /proc/sys/kernel/sysrq && \
-echo b > /proc/sysrq-trigger && \
+
+# Download CHR image
+wget https://github.com/knownrdx/mikrotik/raw/main/chr-7.15.3.img.zip -O chr-7.15.3.img.zip || {
+    echo "Download failed!"
+    exit 1
+}
+
+# Unzip image
+unzip chr-7.15.3.img.zip || {
+    echo "Unzip failed!"
+    exit 1
+}
+
+# Identify storage device
+STORAGE=$(lsblk -dn -o NAME,TYPE | awk '$2 == "disk" {print $1; exit}')
+echo "STORAGE is /dev/$STORAGE"
+
+# Identify main network interface
+ETH=$(ip route show default | awk '/default/ {print $5}')
+echo "ETH is $ETH"
+
+# Get IP address
+ADDRESS=$(ip addr show "$ETH" | grep 'inet ' | awk '{print $2}' | head -n 1)
+echo "ADDRESS is $ADDRESS"
+
+# Get gateway
+GATEWAY=$(ip route show default | awk '{print $3}')
+echo "GATEWAY is $GATEWAY"
+
+sleep 5
+
+# Confirm before flashing
+read -p "WARNING: This will erase /dev/$STORAGE. Continue? (y/N): " confirm
+if [[ "$confirm" != "y" ]]; then
+    echo "Aborted."
+    exit 0
+fi
+
+# Write image to disk
+dd if=chr-7.15.3.img of=/dev/"$STORAGE" bs=4M oflag=sync status=progress || {
+    echo "Failed to write image!"
+    exit 1
+}
+
+echo "Image written successfully. Rebooting in 5 seconds..."
+sleep 5
+
+# Force reboot
+echo 1 > /proc/sys/kernel/sysrq
+echo b > /proc/sysrq-trigger
